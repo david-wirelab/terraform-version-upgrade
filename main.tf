58447196e4ce7073cc47a2fec85c40ff2c2c170d
David Takacs <david.takacs@loxon.eu>
2021-05-21 13:41:04 +0100

    remove previous version

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ terraform {
 
 provider "aws" {
   region     = "eu-west-2"
-  version    = "~> 2.70"
   access_key = var.KEY    # this is a line that should be changed during the upgrade
   secret_key = var.SECRET # this is a line that should be changed during the upgrade
 }
