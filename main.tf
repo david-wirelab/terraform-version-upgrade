f20165e3d77a351f0647774fa7460a1fbed0775a
David Takacs <david.takacs@loxon.eu>
2021-05-21 12:54:15 +0100

    upgrade to v12.0

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
-terraform {
-  backend "s3" {}
-}
-
 provider "aws" {
   region     = "eu-west-2"
   version    = "~> 2.70"
-  access_key = "${var.KEY}"    # this is a line that should be changed during the upgrade
-  secret_key = "${var.SECRET}" # this is a line that should be changed during the upgrade
+  access_key = var.KEY    # this is a line that should be changed during the upgrade
+  secret_key = var.SECRET # this is a line that should be changed during the upgrade
 }
 
 locals {
   naming_suffix = "tf-upgrade"
 }
+
